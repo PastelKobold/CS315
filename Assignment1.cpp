568ad3d8c2e1a73a23e2d1d628e3f3e91b2e91a7
PastelKobold <103293322+PastelKobold@users.noreply.github.com>
2023-02-03 12:27:18 -0500

    Assignment 1 set up

---
+++ Assignment1.cpp
@@ -0,0 +1,39 @@
+/*Assignment 1 sorting
+Implement three sorting algorithms: Insertion Sort, Quicksort, and Merge Sort. 
+Sort data sets that contain data on Pokemon and their total stats. 
+In total, 9 datasets that contain various subsets of the Pokemon dataset in various orders. 
+I have given you a small dataset, a medium dataset, and a large dataset. 
+Each of these also comes in a sorted, reverse sorted, and random variety.
+Estimate the runtime of each algorithm on each dataset by counting the number of comparisons made
+during execution*/
+
+#include <stdio.h>
+#include <Assignment1.h>
+
+//Not sure what number of comparisons means? Maybe each time it compares to switch them? Simple counter will work
+//Counters name is runtime
+
+void Insertion(){
+    //Simple insertion sort
+    
+}
+
+void Quick(){
+    //Need to study quick sort
+
+}
+
+//Will take the longest to code due to how many substats
+void Merge(){
+    //Could use recurrsion or a for loop
+    if(length >= 2){
+        Poke.stats[length/2];
+        //Then take each group and sort each group. Then merge the groups together
+    }
+    else
+        return Merge()
+}
+
+Object Poke(int atk[], int spatk[], int def[], int spdef[], int spd[], int names[]){
+    
+}
---
+++ Assignment1.h
@@ -0,0 +1,23 @@
+//Header for assignment 1
+//public
+
+void Insertion(){
+
+}
+
+void Quick(){
+
+}
+
+void Merge(){
+
+}
+
+//Object will contain atk, spatk, def, spdef, and speed stats. Will also contain names
+Object dataPoke{
+}
+
+//private
+int MAX = 99;
+int dataset1;
+int runtime = 0;
